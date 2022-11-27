c5c4392759813bc6d8053ab2c53b1e1b5487dc33
Joshua Tenorio <joshliam09@gmail.com>
2022-11-26 22:50:18 -0700

    plotting WIP

--- Mockup/client/src/components/TeamStats.js
+++ Mockup/client/src/components/TeamStats.js
@@ -1,5 +1,7 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect, useState } from 'react';
 import {motion} from "framer-motion"
+import { ScatterChart, Scatter, XAxis, YAxis, CartesianGrid, Label } from 'recharts';
+
 
 const TeamStats = () => {
 
@@ -13,10 +15,42 @@ const TeamStats = () => {
             y: "100%"
         }
     };
-    
-    // scatter plot
-    // dropdown menu w/ x axis
-    // update plot whenever dropdown changes
+
+    const [teamData, setTeamData] = useState([]);
+
+    const [rerender, setRerender] = useState(0);
+
+    const [data, setData] = useState([]);
+
+    const getTeamData = async() => {
+        try {
+            const response = await fetch("http://localhost:5000/teams");
+            const jsonData = await response.json();
+
+            setTeamData(jsonData);
+        } catch (err) {
+            console.error(err.message);
+        }
+    };
+    function addData(){
+        setData([]);
+        for(let i = 0; i < teamData.length; i++){
+            let pair = {
+                x: teamData[i]["fdragon"],
+                y: teamData[i]["wins"]/teamData[i]["games"]
+            };
+            setData(data.push(pair));
+        }
+        console.log(data);
+
+        setRerender(rerender + 1);
+    }
+
+    useEffect( ()=> {
+        getTeamData();
+        
+    }, []);
+
     return (
         <motion.div 
         initial={"out"} 
@@ -26,6 +60,24 @@ const TeamStats = () => {
         >
         <Fragment>
             <h2>Team Stats</h2>
+            <button
+                type="button"
+                class="btn btn-primary"
+                onClick={addData}>
+                Add Data
+            </button>
+            <div class="centered">
+            <ScatterChart width={400} height={400}>
+                <CartesianGrid/>
+                <XAxis type="number" dataKey={"x"}>
+                    <Label value="x axis" position="insideBottom" fill='white' offset={0} />
+                </XAxis>
+                <YAxis type="number" dataKey={"y"}>
+                <Label value="Win Rate" position="insideLeft" angle={-90} fill='white'/>
+                </YAxis>
+                <Scatter data={data} fill="white" />
+            </ScatterChart>
+            </div>
         </Fragment>
         </motion.div>
     )
