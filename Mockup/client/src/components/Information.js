7cccdb60aef6c44203c34ecf9f98b1dcd4574787
Joshua Tenorio <joshliam09@gmail.com>
2022-11-30 22:56:34 -0700

    update info

--- Mockup/client/src/components/Information.js
+++ Mockup/client/src/components/Information.js
@@ -24,8 +24,8 @@ const Information = () => {
             <h2>Information</h2>
             For users:
             <ul>
-                <li>Team Stats is where you can compare win rates against objective rates</li>
-                <li>Game Stats is where you can </li>
+                <li>Team Stats is where you can compare win rates against objective rates.</li>
+                <li>Game Stats is where you can view a summary of objective rates and team statistics.</li>
                 <li>Databases is where you can view the raw data.
                     It is also possible to add filters to the data to narrow down a search. </li>
             </ul>
